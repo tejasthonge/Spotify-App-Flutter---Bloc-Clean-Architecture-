f89f463969cf28f62af44d8e28ce03739de73807
TEJAS RAJENDRA THONGE <80546718+tejasthonge@users.noreply.github.com>
2024-07-12 12:18:20 +0530

    crateting the clean arcthtecture and adding the core like app colors and them and assets are done

---
+++ spotify/lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const primary = Color(0xff42C83C); // primary color app color   
+  static const lightBackground = Color(0xffF2F2F2);  // light background for light mode
+
+  static const darkBackground = Color(0xff0D0C0C); // dark background for dark mode
+  static const grey = Color(0xffBEBEBE);    // grey for text
+  static const darkGrey = Color(0xff343434);   // grey for text
+}
